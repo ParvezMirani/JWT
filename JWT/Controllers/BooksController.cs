98858836488873de3bae6df4f81cf685aca1e505
Miraniparvez Khojamusalman <prvz12@hotmail.com>
2018-08-16 14:34:02 +1200

    Add project files.

---
+++ JWT/Controllers/BooksController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace JWT.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        [HttpGet, Authorize]
+        public IEnumerable<Book> Get()
+        {
+            var currentUser = HttpContext.User;
+            int userAge = 0;
+            var resultBookList = new Book[]
+            {
+            new Book { Author = "Ray Bradbury", Title = "Fahrenheit 451", AgeRestriction = false },
+              new Book { Author = "Gabriel García Márquez", Title = "One Hundred years of Solitude", AgeRestriction = false },
+              new Book { Author = "George Orwell", Title = "1984", AgeRestriction = false },
+              new Book { Author = "Anals Nin", Title = "Delta of Venus", AgeRestriction = true }
+           };
+            
+            if(currentUser.HasClaim(c=>c.Type== ClaimTypes.DateOfBirth))
+            {
+                DateTime birtDate = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == ClaimTypes.DateOfBirth).Value);
+                userAge = DateTime.Today.Year - birtDate.Year;
+            }
+            if (userAge < 18)
+            {
+                resultBookList = resultBookList.Where(b => !b.AgeRestriction).ToArray();
+            }
+           return resultBookList;
+        }
+    }
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public bool AgeRestriction { get; set; }
+    }
+}
---
+++ JWT/Controllers/TokenController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+namespace JWT.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        private readonly IConfiguration _config;
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult CreateToken([FromBody]LoginModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = Authenticate(login);
+            if(user!=null)
+            {
+                var tokenString = BuildToken(user);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+        private UserModel Authenticate(LoginModel login)
+        {
+            UserModel user = null;
+            if(login.Username=="mario"&& login.Password=="secret")
+            {
+                string iDate = "1995-05-05";
+                user = new UserModel { Name = "Mario Rossi", Email = "mario.rossi@domain.com", Birthdate= DateTime.Parse(iDate) };
+            }
+            return user;
+        }
+
+        private string BuildToken(UserModel user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,user.Name),
+                new Claim(JwtRegisteredClaimNames.Email,user.Email),
+                new Claim(JwtRegisteredClaimNames.Birthdate,"1991-05-01"),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("veryVerySecretKey"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                expires: DateTime.Now.AddMinutes(5),
+                claims:claims,
+                signingCredentials: creds);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public class LoginModel
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        private class UserModel
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public DateTime Birthdate { get; set; }
+        }
+    }
+}
